7c0d84d24940beeac0187ad63ab9f15c91089c78
itsAkshayDubey <akshay.dubey055@gmail.com>
2021-07-04 21:35:01 +0530

    first commit

---
+++ src/main/java/com/example/xweatherappx/XweatherappxApplication.java
@@ -0,0 +1,15 @@
+package com.example.xweatherappx;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XweatherappxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XweatherappxApplication.class, args);
+	    
+	}
+
+}
---
+++ src/test/java/com/example/xweatherappx/XweatherappxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xweatherappx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XweatherappxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
